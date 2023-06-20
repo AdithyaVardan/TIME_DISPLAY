5989af347f6a1a6544924a43707f531acda92752
Adithya Vardan <a.adithyavardan1@gmail.com>
2023-06-20 14:53:53 +0530

    branch RTC module

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,71 @@
-#include <Arduino.h>
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include "RTClib.h"
+#include <SPI.h>
 #include <string.h>
+#include <iostream>
+RTC_DS1307 DS1307_RTC;
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
-#include <iostream>
-#include <WiFi.h>
-#include <string>
-#include "time.h"
-#include "Ticker.h"
+using namespace std;
 
-const char* ssid     = "your SSID NAME";
-const char* password = "your SSID PASSWORD";
-
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 19850;
-const int   daylightOffset_sec = 0;
 
 // Uncomment according to your hardware type
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+//#define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
+
+// Defining size, and output pins
 #define MAX_DEVICES 4
 #define CS_PIN 5
-char timeHour[4];
-char timeMin[4];
-String s;
-int i=0;
-bool f=true;
+String Date="Jun 20 2023";
+String Time="Jun 20 2023";
 MD_Parola Display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
-String printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return "ERR";
-  }
-  Serial.println(&timeinfo, "   %H:%M:%S  %d  %B  %Y");
-  s="";
-  strftime(timeMin,4,"%M",&timeinfo);
-  strftime(timeHour,4, "%I", &timeinfo);
-  if(f==true){
-    s=strcat(strcat(timeHour,":"),timeMin);
-    f=false;
-  }
-  else
-  {
-    s=strcat(strcat(timeHour," "),timeMin);
-    f=true;
-  }
-  Serial.println(s);
-  Serial.println(i);
-  return s;
-  }
-void setup(){
+
+char Week_days[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
   Serial.begin(115200);
-
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  
-  // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
-
   Display.begin();
   Display.setIntensity(10);
   Display.displayClear();
-}
-void loop(){
-  // printLocalTime();
-  if (Display.displayAnimate()) {
-    Display.displayReset();
-  }
-  Display.setTextAlignment(PA_CENTER);
-  Display.print(printLocalTime());
 
-  delay(1000);
+#ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (!DS1307_RTC.begin()) {
+    Serial.println("Couldn't find RTC");
+    while(1);
+  }
+   //DS1307_RTC.adjust(DateTime("Jun 20 2023","14:47:30"));
+}
+
+
+void loop () {
+    DateTime now = DS1307_RTC.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(Week_days[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    if (Display.displayAnimate()) {
+    Display.displayReset();
+    }
+    Display.setTextAlignment(PA_CENTER);
+    Display.print(now.minute());
+    Display.print(now.second());
+
+    // calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the future
+    DateTime future (now );
+    delay(1000);
 }
